9161dedc840cc9374636f2e42343cd17d4381bc3
NihalBytes <nrai88824@gmail.com>
2024-12-20 21:43:00 -0800

    update

--- coding in c/helloworld.c
+++ coding in c/helloworld.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(){
-printf("hello world");
-    return 0;
+int main()
+{
+int a,b,c,d,e;
+printf("enter the valu of b");
+scanf("%d",&b);
+printf("enter the valu of c");
+scanf("%d",&c);
+
+a=b+c;
+printf("%d",a);
+return 0;
+
 }
